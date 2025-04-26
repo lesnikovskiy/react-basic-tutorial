0f47324d5cc37b014e8e84ca1dd6f4d07c73c7c5
lesnikovskiy <lesnikovski@gmail.com>
2025-04-26 22:16:07 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
+
+import { CORE_CONCEPTS } from "./concepts-data.js";
+import { useState } from "react";
+import TabContent from "./components/TabContent/TabContent.jsx";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <TabContent selectedTopic={selectedTopic} />
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabContent/TabContent.jsx
@@ -0,0 +1,13 @@
+import { EXAMPLES } from "../../examples_data.js";
+
+export default function TabContent({ selectedTopic }) {
+  return (
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  );
+}
