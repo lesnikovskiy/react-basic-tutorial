48591f9cb5624d1e97a30d9aaa7d3f5677696962
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2025-04-28 10:53:54 +0300

    Use uppercase prop ButtonsContainer as a shortcut

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -20,7 +20,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
-        buttonsContainer="menu"
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
